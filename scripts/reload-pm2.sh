d47faf7169757250bec256baa914312983ea8627
Jack Storror <jackarthurstorror@Jacks-MacBook-Pro.local>
2022-01-19 16:03:33 +0000

    changed file

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -5,4 +5,4 @@ pwd
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-pm2 --name HelloWorld start npm -- start
+pm2 --name acebookDeployW3Team2 start npm -- start
