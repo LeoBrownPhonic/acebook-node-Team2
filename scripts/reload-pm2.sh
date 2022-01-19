7a4b1883df9575a64c2eaf7751a5753a48712b0e
Abdur Rehman <wabdulrehman@yahoo.com>
2022-01-19 15:33:28 +0000

    Adding a path to nvm

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -2,4 +2,7 @@
 pwd
 cd /home/ec2-user/acebookDeployW3Team2
 pwd
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 npm start
