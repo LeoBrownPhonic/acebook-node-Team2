5a39023f3e83715c3515c491bac972577f466cf1
Daniel <daniel.szabo09@gmail.com>
2022-01-20 11:11:25 +0000

    test12

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-pwd
 cd /home/ec2-user/acebookDeployW3Team2
 pwd
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-npm install pm2 -g 
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# npm install pm2 -g 
 pm2 --name acebookDeployW3Team2 start npm -- start
