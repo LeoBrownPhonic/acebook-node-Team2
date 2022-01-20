015bbfb731f9da570b56b3aea3ec433a65715998
LeoBrownPhonic <leo_brown_1@hotmail.com>
2022-01-20 16:53:32 +0000

    try old script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -3,5 +3,5 @@ cd /home/ec2-user/acebookDeployW3Team2
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# npm install pm2 -g
+npm install pm2 -g
 pm2 --name acebookDeployW3Team2 start npm -- start
